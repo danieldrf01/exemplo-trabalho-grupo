d963c9475afe518df1d1f8d3ad0e6b81e022d3ce
Alan eduardo alves <alaneduardoalves2018@gmail.com>
2018-05-03 14:08:37 -0300

    inicialização do repositório com mensagem

---
+++ NossaAplicacao/NossaAplicacao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NossaAplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EU \"Daniel\" quero imprimir");
+        }
+    }
+}
